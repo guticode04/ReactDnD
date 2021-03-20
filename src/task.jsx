fdda8b67ebba4c04ffece34632d7033bf1ec36b2
Gustavo Gutierrez <gusguti04@gmail.com>
2021-03-19 20:57:36 -0700

    Add draggable to task

--- src/task.jsx
+++ src/task.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Draggable } from 'react-beautiful-dnd';
 
 const Container = styled.div`
    border: 1px solid lightgrey;
    border-radius: 2px;
    padding: 10px;
    margin-bottom: 10px;
+   background-color: white;
 `;
 
 export default class Task extends React.Component {
    render() {
       return (
-         <Container>
-            {this.props.task.content}
-         </Container>
+         <Draggable draggableId={this.props.task.id} index={this.props.index}>
+            { (provided) => (
+               <Container
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  ref={provided.innerRef}
+               >
+                  {this.props.task.content}
+               </Container>
+            )}
+         </Draggable>
       )
    }
 }
